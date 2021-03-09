8f07510dae08e3bb3407bd4ab9a70832cd10b5d6
Neolamer <rickdecard404@gmail.com>
2021-03-09 21:11:59 +0200

    optimization of the program

---
+++ src/algo/lab1/Gauss.java
@@ -0,0 +1,98 @@
+package algo.lab1;
+
+import java.util.Scanner;
+
+public class Gauss {
+
+    public double[][] initMatrix(int clm, int str){
+        Scanner sc = new Scanner(System.in);
+
+        double [][] array = new double[clm][str];
+
+        System.out.println("WARNING: if your Matrix (with answers) is not 3x4,\n" +
+                             "the program will crash.\n");
+
+        for(int i = 0; i < clm; i++){
+            for(int j = 0; j < str; j++){
+                System.out.format(" a[%d][%d]: ", i, j);
+                array[i][j] = sc.nextDouble();
+            }
+        }
+        System.out.println("____________________________________________________________________");
+        return array;
+    }
+
+    public void printMatrix(double [][]array, int clm, int str){
+        System.out.println("  Array: ");
+        for(int i = 0; i < clm; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < str; j++) {
+                System.out.format(" a[%d][%d]: ", i, j);
+                System.out.format("%.2f",array[i][j]);
+                System.out.print("\t");
+            }
+        }
+        System.out.print("\n");
+        System.out.println("____________________________________________________________________");
+    }
+
+    public void checkResult(double [][] matrixDflt, double [][] matrixRslt){
+        System.out.println(" Checking results: ");
+
+        System.out.println(" Input Matrix: ");
+
+        for(int i = 0; i < 3; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < 4; j++) {
+                System.out.format("%.2f", matrixDflt[i][j]);
+                System.out.print("\t");
+            }
+        }
+        System.out.print("\n");
+
+        System.out.println("____________________________________________________________________");
+        System.out.println("Results: ");
+
+        double sum = 0;
+        double [] Result = new double[3];
+        double [] Input = new double[3];
+        double value;
+
+        System.out.print("\n");
+
+        for(int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Input[i] = matrixDflt[i][3];
+                System.out.print("\t");
+                value = matrixRslt[j][3] * matrixDflt[i][j];
+                sum += value;
+                Result[i] = sum;
+            }
+            System.out.format("%.2f", sum);
+            System.out.print("\t");
+            sum = 0;
+        }
+        System.out.print("\n");
+        System.out.println("Task is done");
+    }
+
+    public void determinant(double [][]array){
+        double determinant;
+        double x,y,z;
+        double[][] matrix = new double[3][3];
+
+        for(int i = 0; i < 3; i++){
+            System.arraycopy(array[i], 0, matrix[i], 0, 3);
+        }
+
+        x = (matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]));
+        y = (matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]));
+        z = (matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]));
+        determinant = x - y + z;
+
+        System.out.print(" Determinant of the Matrix: ");
+        System.out.format("%.2f", determinant);
+        System.out.print("\n");
+        System.out.println("____________________________________________________________________");
+    }
+}
---
+++ src/algo/lab1/GaussSolution.java
@@ -0,0 +1,128 @@
+package algo.lab1;
+
+public class GaussSolution {
+
+    double[][] arrayDefault = {{0.80, 1.61, 3.76, 1.75},
+            {2.19, 1.26, 0.94, 1.29},
+            {0.93, 2.38, 4.02, -2.50}};
+
+    double[][] array;
+
+    public void calcGauss(boolean modeDefault) {
+
+        System.out.println("\tYasinGauss");
+
+        Gauss gauss = new Gauss();
+        System.out.println("____________________________________________________________________");
+
+        if (modeDefault) {
+            System.out.println("____________________________________________________________________");
+            System.out.println(" >>> Creating Matrix for variant 26: \n");
+            gauss.printMatrix(arrayDefault, 3, 4);
+            gauss.determinant(arrayDefault);
+            array = arrayDefault;
+        } else {
+            System.out.println("____________________________________________________________________");
+            array = gauss.initMatrix(3, 4);
+            gauss.printMatrix(array, 3, 4);
+        }
+
+        double[] tempLine = new double[4];
+
+        double[][] tempArray = new double[3][4];
+        for (int i = 0; i < 3; i++) {
+            System.arraycopy(array[i], 0, tempArray[i], 0, 4);
+        }
+
+        System.out.println(" Divide the first line by a[0][0]:");
+        double tempValue = tempArray[0][0];
+        for (int j = 0; j < 4; j++) {
+            tempArray[0][j] /= tempValue;
+        }
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println(" From the 2 line subtract the 1 line multiplied by [1][0]");
+        tempValue = tempArray[1][0];
+        for (int j = 0; j < 4; j++) {
+            tempLine[j] = tempArray[0][j] * tempValue;
+        }
+        for (int j = 0; j < 4; j++) {
+            tempArray[1][j] -= tempLine[j];
+        }
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println(" From the 3 line subtract the 1 line multiplied by [2][0]");
+
+        tempValue = tempArray[2][0];
+        for (int j = 0; j < 4; j++) {
+            tempLine[j] = tempArray[0][j] * tempValue;
+        }
+        for (int j = 0; j < 4; j++) {
+            tempArray[2][j] -= tempLine[j];
+        }
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println("Divide the 2 line by [1][1]");
+
+        tempValue = tempArray[1][1];
+        for (int j = 0; j < 4; j++) {
+            tempArray[1][j] /= tempValue;
+        }
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println("From the 1 line subtract the 2, multiplied by [0][1]");
+
+        tempValue = tempArray[0][1];
+        for (int j = 0; j < 4; j++) {
+            tempLine[j] = tempArray[1][j] * tempValue;
+        }
+        for (int j = 0; j < 4; j++) {
+            tempArray[0][j] -= tempLine[j];
+        }
+
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println("From the 3 line subtract the 2, multiplied by [1][2]");
+
+        tempValue = tempArray[2][1];
+        for (int j = 0; j < 4; j++) {
+            tempLine[j] = tempArray[1][j] * tempValue;
+        }
+        for (int j = 0; j < 4; j++) {
+            tempArray[2][j] -= tempLine[j];
+        }
+
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println("Divide the 3 line by [2][2]");
+        tempValue = tempArray[2][2];
+        for (int j = 0; j < 4; j++) {
+            tempArray[2][j] /= tempValue;
+        }
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println("From the 1 line subtract the 3, multiplied by [0][2]");
+        tempValue = tempArray[0][2];
+        for (int j = 0; j < 4; j++) {
+            tempLine[j] = tempArray[2][j] * tempValue;
+        }
+        for (int j = 0; j < 4; j++) {
+            tempArray[0][j] -= tempLine[j];
+        }
+        gauss.printMatrix(tempArray, 3, 4);
+
+        System.out.println("From the 2 line subtract the 3, multiplied by [1][2]");
+        tempValue = tempArray[1][2];
+        for (int j = 0; j < 4; j++) {
+            tempLine[j] = tempArray[2][j] * tempValue;
+        }
+        for (int j = 0; j < 4; j++) {
+            tempArray[1][j] -= tempLine[j];
+        }
+
+        gauss.printMatrix(tempArray, 3, 4);
+        System.out.print("\n");
+
+        gauss.checkResult(array, tempArray);
+    }
+}
